444fa193cfecdd772cac68daaa3a6451a1c4068f
iamhan <ahmadrayhan87@gmail.com>
2024-03-16 04:34:52 +0700

    tugas-mvc-crud

---
+++ tugas-07032024-msib-ahmad-rayhan/database/migrations/2024_03_15_182921_create_tbl_akuns_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_akuns', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('username');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('password');
+            $table->string('role');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_akuns');
+    }
+};
---
+++ tugas-07032024-msib-ahmad-rayhan/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('/addcar',[CarController::class,'index']);
+Route::post('/addcar',[CarController::class,'tambahmobil']);
+Route::get('/editmobil/{id}',[CarController::class,'editmobil']);
+Route::post('/editmobil',[CarController::class,'edit']);
+Route::get('/deletee/{id}',[CarController::class,'delete']);
