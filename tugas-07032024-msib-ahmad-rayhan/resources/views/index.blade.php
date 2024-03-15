7762bab4f53661765a8ee0f9b94923815dc31f2d
iamhan <ahmadrayhan87@gmail.com>
2024-03-16 04:15:28 +0700

    update

--- tugas-07032024-msib-ahmad-rayhan/resources/views/index.blade.php
+++ tugas-07032024-msib-ahmad-rayhan/resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HiCar Rentals</title>
+    <title>MyCar Rental</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/P.png') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
